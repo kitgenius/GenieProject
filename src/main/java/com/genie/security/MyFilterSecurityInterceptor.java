702372d9dd544274a1eb8def3c8d09b98a2b0a17
kitgenius <kitgenius@163.com>
2018-02-23 23:39:45 +0800

    修改spring-security配置。增加MyUserDetailService类，根据用户名查询角色名，并返会User。修改MyFilterSecurityInterceptor类，增加doFilter处理。

--- src/main/java/com/genie/security/MyFilterSecurityInterceptor.java
+++ src/main/java/com/genie/security/MyFilterSecurityInterceptor.java
@@ -11,12 +11,18 @@ import javax.servlet.ServletResponse;
 
 import org.springframework.security.access.SecurityMetadataSource;
 import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
+import org.springframework.security.access.intercept.InterceptorStatusToken;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 
 /*author:Genie
  *date:2017年12月16日
 **/
 public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor implements Filter {
 
+	//配置文件注入
+	private FilterInvocationSecurityMetadataSource securityMetadataSource;
+	
 	@Override
 	public Class<?> getSecureObjectClass() {
 		// TODO Auto-generated method stub
@@ -26,7 +32,7 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 	@Override
 	public SecurityMetadataSource obtainSecurityMetadataSource() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.securityMetadataSource;
 	}
 
 	@Override
@@ -38,14 +44,32 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		
+		FilterInvocation fi = new FilterInvocation(request, response, chain);
+		invoke(fi);		
 	}
-
+	
+	public void invoke(FilterInvocation fi) throws IOException,ServletException {
+		InterceptorStatusToken token = super.beforeInvocation(fi);//beforeInvocation方法会调用securityMetadataSource的getAttributes方法获取被拦截url所需的权限
+		try{
+			//继续向下执行下一个拦截器
+			fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
+		}finally{
+			super.afterInvocation(token, null);
+		}
+	}
+	
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public FilterInvocationSecurityMetadataSource getSecurityMetadataSource() {
+		return securityMetadataSource;
+	}
+
+	public void setSecurityMetadataSource(FilterInvocationSecurityMetadataSource securityMetadataSource) {
+		this.securityMetadataSource = securityMetadataSource;
 	}
 
 }
---
+++ src/main/java/com/genie/security/MyUserDetailService.java
@@ -0,0 +1,48 @@
+package com.genie.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.genie.dao.HibernateDao;
+import com.genie.entity.Login;
+import com.genie.entity.Role;
+
+/*author:Genie
+ *date:2018年2月23日
+**/
+public class MyUserDetailService implements UserDetailsService {
+
+	@Autowired
+	private HibernateDao hibernateDao;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Login login = (Login) hibernateDao.findByProperty(Login.class, Restrictions.eq("username", username)).get(0);
+		Collection<Role> roles = login.getRole();
+		
+		Collection<GrantedAuthority> auths = null;
+		if(login != null){
+			auths = new ArrayList<GrantedAuthority>();
+			if(roles.isEmpty() == false){
+				for(Role role:roles){
+					auths.add(new SimpleGrantedAuthority(role.getRoleName()));
+				}
+			}
+		}
+		
+		User user = new User(username, login.getPassword(), true, true, true, true, auths);
+		
+		return user;
+	}
+	
+
+}
